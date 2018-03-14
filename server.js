bf3b7b8646a7ed4f53bde145b77eea2b85d7b408
Rory McGuire <rory@finkube.com>
2018-03-14 01:19:15 -0500

    Added mobile and pc pages.

--- server.js
+++ server.js
@@ -7,19 +7,36 @@
  */
 
 
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var fs = require('fs'); //require filesystem module
 
-http.listen(8080); //listen to port 8080
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/home.htm');
+});
+
+app.get('/pc', function(req, res){
+  res.sendFile(__dirname + '/public/homepc.htm');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
-}
+
+});
+
+
+
+io.on('disconnect', function(socket){
+  console.log('a user disconnected');
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
